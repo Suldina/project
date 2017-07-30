0ed3a1271b5efc901d8eb87bf2e10314a993f9d1
Suldina <elenasuldina@mail.ru>
2017-07-30 17:20:14 +0300

    примерный алгоритм

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -18,42 +18,43 @@ private static String read() throws IOException {
         return console().readLine();
         }
 public void ParsingAndCalculate(string str){
-        StringBuilder intemed  = new StringBuilder();
+        StringBuilder intermed  = new StringBuilder();
         double argum1=0; //накопительная переменная для результата
-        char sign=' '; //знак
+        char sign=' '; //арифметический знак
 //        int state=0;  //  режим, 0 - чтение первого числа, 1 - чтение знака, 2 - чтение второго числа
         int i=0;
         string str=" ";
 
-        for (int i=0; str.lenght(), i++){
-            while(str[i]not in {'+','-','*','/'} // пока не встретим арифм. знак операции, не знаю, как синтаксически пишется
-            {intermed.append(intermed); // добавляем  в конец строки  intermed следующую цифру
-            i++;
-            }
-            sign=str[i]; // записываем в sign текущий символ
-            switch (sign) {
-                case "+":
-                return argum1=argum1 + intermed;
-                case "-":
-                return argum1=argum1 - intermed;
-                case "*":
-                return argum1=argum1 * intermed;
-                case "/":
-                if (intermed != 0) {
-                    return argum1=argum1 / intermed;
+            for (int i=0; str.lenght(), i++){
+                while(str[i]not in {'+','-','*','/'} // пока не встретим арифм. знак операции, не знаю, как синтаксически пишется
+                {intermed.append(intermed); // добавляем  в конец строки  intermed следующую цифру
+                i++;
                 }
-                else {
-                    System.out.println("Деление на ноль невозможно!");
-                  break;
-                }
+                sign=str[i]; // записываем в sign текущий символ
+                switch (sign) {
+                    case "+":
+                    return argum1=argum1 + intermed;
+                    case "-":
+                    return argum1=argum1 - intermed;
+                    case "*":
+                    return argum1=argum1 * intermed;
+                    case "/":
+                    if (intermed != 0) {
+                        return argum1=argum1 / intermed;
+                    }
+                    
+                    else {
+                        System.out.println("Деление на ноль невозможно!");
+                      break;
+                    }
 
 
 
-            break;
-            }argum1=intermed; // записываем в argum1 промежуточное значение intermed
-            intermed=0; //обнуляем intermed
+                break;
+                }argum1=intermed; // записываем в argum1 промежуточное значение intermed
+                intermed=0; //обнуляем intermed
             }
-        System.out.println(
+        System.out.println(argum1);
 
 
         }
