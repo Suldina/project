38a8e41051f50c6212390753fc0c1ee4d379c6ee
Suldina <elenasuldina@mail.ru>
2017-08-02 00:10:54 +0300

    примерный алгоритм

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,37 @@
-import java.io.InputStreamReader;
+package com.company
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) { // входная точка
-        // создаем объект Calc, передаем ему читателя вокруг system.in
-        // и тут же запускаем цикл чтения/печати
-        new Calc(new InputStreamReader(System.in)).run();
+public class ConsoleCalc {
+    Calc calc= new Calc ();
+    double frst = 0;
+    double scnd = 0;
+    String sign = '';
+    double res=0;
+
+    BufferedReader reader= new Bufferedreader (new InputStreamReader (System.in));
+    try {
+        frst =Double.parseDouble(reader.readLine());
+        scnd=Double.parseDouble(reader.readLine());
+        sign= reader.readLine();
+    } catch (NumberFormatException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+    switch (sign) {
+        case "+":
+            res=calc.plus (frst, scnd);
+            break;
+        case "-":
+            res=calc.minus (frst, scnd);
+            break;
+        case "*":
+            res=calc.multiply (frst, scnd);
+            break;
+        case "/":
+            res=calc.div (frst, scnd);
+            break;
 
-}
+    }
